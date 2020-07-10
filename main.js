7a2ed66d87b164ab3a08d617e226ed2e4a7055b9
gourav221b <57912157+gourav221b@users.noreply.github.com>
2020-07-10 21:51:46 +0530

    loader added

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 AOS.init();
 
+$(window).on("load", function() {
+    $(".loader-container").fadeOut(1000);
+    $("body").scroll(0);
+});
+
 function openSide() {
     document.getElementById("menu").style.width = "150px";
     document.getElementById("content").style.marginLeft = "150px";
